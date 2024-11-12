7352868109b178947bdb46a39adee769c91acfc2
amanasati <aman.asati@samyotech.com>
2024-11-12 15:32:53 +0530

    updated employee model

--- src/models/employee.js
+++ src/models/employee.js
@@ -6,19 +6,19 @@ const employee = new Schema(
   {
     firstName: {
       type: String,
-      required: true,
+      
     },
     lastName: {
       type: String,
-      required: true,
+      
     },
     email: {
       type: String,
-      required: true,
+      
     },
     phone: {
       type: Number,
-      required: true,
+      
     },
     status: {
       type: String,
